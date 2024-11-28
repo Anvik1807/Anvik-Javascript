88b937f0087f8ef863ac6f6e036ed4f43c33de4b
Your Name <you@example.com>
2024-11-28 08:52:38 -0500

    function

--- employeeDetails/employee_details.js
+++ employeeDetails/employee_details.js
@@ -1,7 +1,7 @@
 const employees = [
-      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
+      { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization:'Javascript'},
+      { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000,specialization:'Javascript2' },
+      { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000,specialization:'123' },
       //... More employee records can be added here
     ];
 function displayEmployees() {
@@ -29,4 +29,18 @@ function displayEmployees() {
       document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this ID';
 
     }
+}
+
+function findBySpecialization(specialization){
+    const foundEmployee = employees.find(employee => employee.specialization === specialization);
+    console.log(foundEmployee);
+    if(foundEmployee){
+        //alert(`<p>${foundEmployee.id}<p>`);
+        //document.getElementById('employeesDetails').innerHtml = 'hello';
+        document.getElementById('employeesDetails').innerHTML = `<p>${foundEmployee.id}: ${foundEmployee.name}: ${foundEmployee.name} - ${foundEmployee.department} - $${foundEmployee.salary}</p>`;
+    }
+    else{
+        document.getElementById('employeesDetails').innerHTML = 'no employee has been found with this specialisation';
+  
+      }
 }
